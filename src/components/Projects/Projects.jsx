a6c363fde04efebad0de1a9b1cd209c6ba3f1166
Vishes Keshari <65493591+visheskeshari@users.noreply.github.com>
2023-09-28 17:21:54 +0530

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -57,7 +57,8 @@ const Projects = () => {
       projectImage:
         "https://raw.githubusercontent.com/visheskeshari/ZoomClone/main/screenshots/Screenshot%20(134).png",
       projectTitle: "Zoom Clone",
-      description: "Real-time audio-video communication on the model ZOOM.",
+      description:
+        "Real-time audio-video-text communication application based on the model of ZOOM. It uses WebRTC and Socket.io that enables low-latency, bidirectional, and event-based communication between a client and a server.",
       technologies: "HTML, CSS, JavaScript, NodeJS",
     },
     {
@@ -67,7 +68,7 @@ const Projects = () => {
         "https://raw.githubusercontent.com/visheskeshari/Wateriot/main/Flow%20Diagram.png",
       projectTitle: "Watertiot: advance IoT based Smart Water Purifier",
       description:
-        "This invention was a smart water purifier that works on Raspberry Pi and Thing Speak Cloud. The main objective of it was to use concepts of Internet of Things and various sensors in different segments of the purifier to monitor and control their working in real-time.",
+        "This invention was a smart water purifier that works on Raspberry Pi and Thing Speak Cloud. The main objective of it was to use concepts of the Internet of Things and various sensors in different segments of the purifier to monitor and control their working in real-time.",
       technologies: "Internet of Things, Raspberry Pi and ThingSpeak Cloud",
     },
     {
@@ -77,7 +78,7 @@ const Projects = () => {
         "https://raw.githubusercontent.com/visheskeshari/Portfolio/main/src/images/portfolio.png",
       projectTitle: "React.js based Portfolio",
       description:
-        "The Portfolio web app is a responsive & dynamic website that provides an engaging user interface and a visually appealing presentations of my projects, skills, contact information, and additional details.",
+        "The Portfolio web app is a responsive & dynamic website that provides an engaging user interface and visually appealing presentations of my projects, skills, contact information, and additional details.",
       technologies:
         "React.js, Node.js, HTML, CSS, Javascript, Express.js, Three.js",
     },
